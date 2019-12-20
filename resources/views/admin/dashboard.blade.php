049c768fd995778c0ff1ba78b5a969d95d117493
Alexey Aleynikov <aleinikov376443@gmail.com>
2019-12-20 19:13:08 +0300

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.layouts.app_admin')
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="jumbotron">
+                <p><snap class="label label-primary">Категорий 0</snap></p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="jumbotron">
+                <p><snap class="label label-primary">Матералов 0</snap></p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="jumbotron">
+                <p><snap class="label label-primary">Посетителей 0</snap></p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="jumbotron">
+                <p><snap class="label label-primary">Сегодня 0</snap></p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <a class="btn btn-block btn-default" href="#">Создать категорию</a>
+            <a class="list-group-item" href="#">
+                <h4 class="list-group-item-heading">Категоря первая</h4>
+                <p class="list-group-item-text">
+                    Количесто материалов
+                </p>
+            </a>
+        </div>
+        <div class="col-sm-6">
+            <a class="btn btn-block btn-default" href="#">Создать материал</a>
+            <a class="list-group-item" href="#">
+                <h4 class="list-group-item-heading">Материал первый</h4>
+                <p class="list-group-item-text">
+                    Категория
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
